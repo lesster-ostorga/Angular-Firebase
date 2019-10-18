ec8363cc20c4ac66e28aea843b6292df65a87c14
lesster-ostorga <lesster3048@hotmail.com>
2019-10-18 08:00:30 -0600

    Servicio
    se agrego un servicio de tipo firebase pero un import me esta causando errro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 
 @NgModule({
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+//import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor() { }
+}
